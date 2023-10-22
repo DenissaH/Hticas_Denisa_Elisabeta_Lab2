794300b55b24d77c22ede706f7999544b99e68b1
hitic <hitic@DESKTOP-994V85S>
2023-10-22 23:22:09 +0300

    Add project files.

---
+++ Data/Hticas_Denisa_Elisabeta_Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Hticas_Denisa_Elisabeta_Lab2.Models;
+
+namespace Hticas_Denisa_Elisabeta_Lab2.Data
+{
+    public class Hticas_Denisa_Elisabeta_Lab2Context : DbContext
+    {
+        public Hticas_Denisa_Elisabeta_Lab2Context (DbContextOptions<Hticas_Denisa_Elisabeta_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Hticas_Denisa_Elisabeta_Lab2.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hticas_Denisa_Elisabeta_Lab2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
